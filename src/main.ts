351d96f28e3b2aca95210e0bd924c3d3b32fd901
Youvprog <jubachabane35@gmail.com>
2025-09-15 14:49:21 +0100

    added swagger config

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 import { CustomValidatorPipe } from './exceptions/custom-validator-pipe';
 import { CustomExceptionFilter } from './exceptions/custom-exception-filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<number>('PORT');
+  const apiVersion = configService.get<string>('API_VERSION');
 
   app.set('trust proxy', 1);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix(`api/${apiVersion}`);
   app.use(helmet());
   app.useGlobalPipes(
     new CustomValidatorPipe({ transform: true, whitelist: true }),
@@ -22,9 +24,21 @@ async function bootstrap() {
   const adatperHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new CustomExceptionFilter(adatperHost));
 
+  if (configService.get<string>('NODE_ENV') === 'dev') {
+    const config = new DocumentBuilder()
+      .setTitle('Project X')
+      .setDescription('API documentation for Project X')
+      .setVersion(`${apiVersion}`)
+      .build();
+    const documentFactory = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory, {
+      customSiteTitle: 'Project X',
+    });
+  }
+
   const logger = new Logger('Main');
   logger.verbose(
-    `API started successfully, listening on port ${port}:: -> http://localhost:${port}/api/v1`,
+    `API started successfully, listening on port ${port}:: -> http://localhost:${port}/api`,
   );
   await app.listen(port ?? 3000);
 }
